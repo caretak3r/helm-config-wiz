8e58423e366fd85bb5cae06d5879b472f7890923
rohit <50377477+caretak3r@users.noreply.github.com>
2025-11-12 09:55:16 -0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/helm-config-wiz/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
